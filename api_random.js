b5b03dda00b139be0e08d2a568f2f4012c8b6259
ViviChimenti <vivichimenti@gmail.com>
2023-03-02 23:49:59 -0300

    prueba 2 dia 2 del 3

--- api_random.js
+++ api_random.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    datos();
+    datos2();
     
     
     
 });
 
 function datos() {
-   
+ 
+
       fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then(data => {
@@ -43,5 +44,48 @@ function datos() {
         });
  
 }
+
+
+function datos2() {
+   
+  fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data => {
+          const user = data.results[0];
+          const userInfo = document.getElementById('user-info');
+          userInfo.innerHTML = 
+          `
+          <div id="imagen_div" ><img id="foto" src="${user.picture.large}" alt="User picture" ></div>
+
+          <div id="info_div">
+            <div id="texto">
+                <div>
+                    <p><span style="color: #182153; font-size: 30px; font-weight: bold;"> ${user.name.first} ${user.name.last} </span><br /><span style="color: #182153;font-size: 20px; font-weight: bold;">Desarrollador</span><p>
+                </div>
+                <div>
+                    <p><span style="color: #182153;font-weight: bold;">Email</span><br> ${user.email}<p>
+                </div>
+                <div>
+                    <p><span style="color: #182153;font-weight: bold;">Teléfono:</span><br> ${user.cell}<p>
+                </div>
+                <div>
+                    <p><span style="color: #182153;font-weight: bold;">Dirección:</span><br> ${user.location.street.name} ${user.location.street.number}<p>
+                </div>
+                <div>
+                    <p><span style="color: #182153;font-weight: bold;">Edad:</span><br> ${user.dob.age}<p>
+                </div>
+            </div>   
+          </div>
+        `;
+         document.getElementById("nombre_h1").innerText= user.name.first +"  "+user.name.last;
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+        });
+ 
+}
+
+
+
 
 
